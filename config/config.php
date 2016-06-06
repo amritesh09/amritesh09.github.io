31cfb75af5efc4a5ca0f6e3e358f4b97c88fa765
Amritesh Srivastava <asriva3@L-IDC24KG3QC-M.local>
2016-06-06 14:52:33 +0530

    added config

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+return array(
+	'adminName'=> 'Amritesh Srivastava',
+	'adminEmail'=> 'srivastava.amritesh09@gmail.com',
+	'adminSubject'=> 'Query Received from AmriteshSrivastava.com',
+	'autoQuerySubject'=> 'We have received your Query! - AmriteshSrivastava.com',
+	);
+
+?>
